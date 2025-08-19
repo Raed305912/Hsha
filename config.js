0db854d94f44874eeef9ddabc5d01d5e223f716d
Raed305912 <saifxd910@gmail.com>
2025-08-19 23:58:51 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ require("dotenv").config();
 module.exports = {
   token: process.env.TOKEN,
   prefix: process.env.PREFIX || "!",
-  owner: process.env.OWNER || "1079022798523093032"
+  owner: process.env.OWNER || "1079022798523093032",
+  clientId: process.env.CLIENT_ID
 };
