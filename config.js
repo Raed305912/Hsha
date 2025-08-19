18731ec31dfecf9546f6ed40debb13a651bafe1b
Raed305912 <saifxd910@gmail.com>
2025-08-19 23:42:20 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  token: "MTM5ODk1MDI1MTc2MjgxNTA4Ng.GKXy4s.xdkIjUrBGZx1VLUTpXqgBduOOMxUA6Pm3yQ2eA",
+  token: "MTM5ODk1MDI1MTc2MjgxNTA4Ng.G7hRzo.dHWF4u-CkNY3NGlKR713X96Go7_rX03FP_m46M",
   prefix: "!",
   owner: "1079022798523093032"
 }; 
